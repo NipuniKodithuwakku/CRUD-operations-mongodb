d466af0e2be067a6634aa0fc968a06d9c1c7ed5c
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-19 17:44:49 +0530

    create a schema

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //connect mongo db
 const mongoose=require('mongoose');
 
-monggose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground')
 //this connect method return a promise , so we call 'then' method
     .then(() => console.log('connected to the mongodb'))
-    .catch((err) => console.error("couldn't connect to the mongodb",err));
+    .catch((err) => console.error("couldn't connect to the mongodb",err))
+
+//we use schema to define  the shape of documents in mongodb
+    const courseSchema = new mongoose.Schema({
+        name : String,
+        author: String,
+        tags: [String],
+        //give default value to the date property
+        date: {type: Date, default: Date.now },
+        isPublished: Boolean
+    });
