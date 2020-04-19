b916a6a35f9854cc33eba22fa238549985180df7
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-19 12:28:31 +0530

    connect to the mongodb

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 //connect mongo db
 const mongoose=require('mongoose');
+
+monggose.connect('mongodb://localhost/playground')
+//this connect method return a promise , so we call 'then' method
+    .then(() => console.log('connected to the mongodb'))
+    .catch((err) => console.error("couldn't connect to the mongodb",err));
