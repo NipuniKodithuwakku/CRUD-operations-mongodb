e9c581990a0e3682dab73e63ca09e71f50aa8149
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-20 21:31:26 +0530

    save another document

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 //connect mongo db
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-//this connect method return a promise , so we call 'then' method
-    .then(() => console.log('connected to the mongodb'))
-    .catch((err) => console.error("couldn't connect to the mongodb",err))
+mongoose
+  .connect("mongodb://localhost/playground")
+  //this connect method return a promise , so we call 'then' method
+  .then(() => console.log("connected to the mongodb"))
+  .catch((err) => console.error("couldn't connect to the mongodb", err));
 
 //we use schema to define  the shape of documents in mongodb
-    const courseSchema = new mongoose.Schema({
-        name : String,
-        author: String,
-        tags: [String],
-        //give default value to the date property
-        date: {type: Date, default: Date.now },
-        isPublished: Boolean
-    });
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  //give default value to the date property
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
 
 //classes,objects
 //course,nodeCourse
 //compile that schema to model
 //first argument is singlar name of the db collection
 
-const Course= mongoose.model('Course',courseSchema)
+const Course = mongoose.model("Course", courseSchema);
 //create an instance of Course class
 
 //since we use async we must put await into async function
-async function createCourse(){
-    const course = new Course({
-    name: 'Node.js Course',
+async function createCourse() {
+  const course = new Course({
+    name: "Angular course",
     author: "Nipuni",
-    tags: ['node','backend'],
-    isPublished: true
-})  
+    tags: ["angular", "frontend"],
+    isPublished: true,
+  });
 
-//saving the course to the document in database
-//since this is an asynchronize activity we must use "await" keyword
+  //saving the course to the document in database
+  //since this is an asynchronize activity we must use "await" keyword
 
-const result = await course.save();
-console.log(result);
-
+  const result = await course.save();
+  console.log(result);
 }
 
 createCourse();
