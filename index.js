09d139fce1ffd6da2d4c0dec63a1dbf14f0d0416
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-30 22:37:17 +0530

    create update first

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const courseSchema = new mongoose.Schema({
   //give default value to the date property
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: Number,
 });
 
 //classes,objects
@@ -24,55 +25,6 @@ const courseSchema = new mongoose.Schema({
 
 const Course = mongoose.model("Course", courseSchema);
 //create an instance of Course class
-
-//since we use async we must put await into async function
-async function createCourse() {
-  const course = new Course({
-    name: "Angular course",
-    author: "Nipuni",
-    tags: ["angular", "frontend"],
-    isPublished: true,
-  });
-
-  //saving the course to the document in database
-  //since this is an asynchronize activity we must use "await" keyword
-
-  const result = await course.save();
-  console.log(result);
-}
-
-//retrieve data from the database
-async function getCourses() {
-  const pageNumber = 2;
-  const pageSize = 10;
-
-  //return a document
-  const courses = await Course.find({ author: "Nipuni", isPublished: true })
-    .find({ price: { $gte: 10, $lte: 20 } })
-    // .find({price:{$in[10,15,20]} })
-    .find()
-    // .or([{author: "Nipuni"},{isPublished: true}])
-    .and([{ author: "Nipuni" }, { isPublished: true }])
-
-    //starts with Nipuni
-    .find({ author: /^Nipuni/ })
-    //ends with Nipuni i represent the case sensivity
-    .find({ author: /Nipuni$/i })
-    //contain in any position of the word
-    .find({ author: /.*Nipuni.*/i })
-
-    //pagination
-    //in real world
-    //api/courses?pageNumber=2&pageSize=10
-    // .skip((pageNumber-1)*pageSize})
-    .limit(pageSize)
-    .sort({ name: 1 })
-    .select({ name: 1, tags: 1 })
-    .count();
-  console.log(courses);
-}
-// createCourse();
-getCourses();
 
 async function updateCourse(id) {
   //approach:query first
@@ -97,4 +49,4 @@ async function updateCourse(id) {
   console.log(result);
 }
 
-updateCourse("5a68fde3f09ad7646ddec17e");
+updateCourse("5a68fdc3615eda645bc6bdec");
