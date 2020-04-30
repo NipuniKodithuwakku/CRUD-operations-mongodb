0bde4fe92cc8257aa636193d2f5869b165fd6154
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-04-30 11:01:18 +0530

    create a solution 3

--- solution3.js
+++ solution3.js
@@ -1,1 +1,28 @@
-//load 
+//load mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/playground");
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: Date,
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = mongoose.model("Course", courseSchema);
+async function getCourses() {
+  return await Course.find({ isPublished: true }).or([
+    { price: { $gte: 15 } },
+    { name: /.*by.*/i },
+  ]);
+}
+
+async function run() {
+  const result = await getCourses();
+  console.log(result);
+}
+
+run();
