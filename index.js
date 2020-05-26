3b3c7f399d31b2af1b7ffdf05ce845314c997fa0
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-26 17:24:19 +0530

    re-arrange the code

--- index.js
+++ index.js
@@ -1,44 +1,6 @@
-//load mongoose
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/playground")
-  //this connect method return a promise , so we call 'then' method
+  .connect("mongodb://localhost/text")
   .then(() => console.log("connected to the mongodb"))
-  .catch((err) => console.error("couldn't connect to the mongodb", err));
-
-//we use schema to define  the shape of documents in mongodb
-const courseSchema = new mongoose.Schema({
-  name: String,
-  author: String,
-  tags: [String],
-  //give default value to the date property
-  date: { type: Date, default: Date.now },
-  isPublished: Boolean,
-  price: Number,
-});
-
-//classes,objects
-//course,nodeCourse
-//compile that schema into model
-//model's first argument is singler name of the db collection
-
-const Course = mongoose.model("Course", courseSchema);
-// async function updateCourse(id) {
-//   const result = await Course.update(
-//     { _id: id },
-//     {
-//       $set: {
-//         author: "Nipuni",
-//         isPublished: false,
-//       },
-//     }
-//   );
-//   console.log(result);
-// }
-// updateCourse("5a68ff090c553064a218a547");
-async function deleteCourse(id) {
-  const result = await Course.deleteOne({ _id: id });
-  console.log(result);
-}
-deleteCourse("5a68ff090c553064a218a547");
+  .catch((err) => console.error("could not connect to the mongodb", err));
