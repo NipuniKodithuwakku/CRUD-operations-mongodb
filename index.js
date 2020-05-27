47e5ed7284cc175757e0f9868c4503207af53896
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-27 12:02:03 +0530

    querying a document

--- index.js
+++ index.js
@@ -9,20 +9,28 @@ const courseSchema = new mongoose.Schema({
   name: String,
   author: String,
   tags: [String],
-  date: Date,
+  date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
 const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
   const course = new Course({
-    name: "Node.js course",
+    name: "Angular.js course",
     author: "Mosh",
-    tags: ["node", "backend"],
+    tags: ["Angular", "frontend"],
     isPublished: true,
   });
 
   const result = await course.save();
   console.log(result);
 }
-createCourse();
+async function getCourses() {
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+
+  console.log(courses);
+}
+getCourses();
