96578ef8d44067c9ce882019e1a2b2ddec48dd8c
Nipuni Kodithuwakku <39560338+NipuniKodithuwakku@users.noreply.github.com>
2020-05-28 03:33:26 +0530

    add count

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ async function createCourse() {
   console.log(result);
 }
 async function getCourses() {
-  const courses = await Course
-    // .find({ author: "Mosh", isPublished: true })
+  const courses = await Course.find({ author: "Mosh", isPublished: true })
     // .find({ price: { $gte: 10, $lte: 20 } })
     // .find()
     //  .or([{ author: "Mosh" }, { isPublished: true }])
     // .and([{ author: "Mosh" }, { isPublished: true }])
-    .find({ author: /.*Mosh.*/ })
+    // .find({ author: /.*Mosh.*/ })
     .limit(10)
-    .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
+    .sort({ name: 1 });
+  // .select({ name: 1, tags: 1 });
+  count();
 
   console.log(courses);
 }
